d3e5096b32ad94708c78af50c5be8eb72b1e1ff1
狠心源 <50833540+2149495141@users.noreply.github.com>
2023-02-20 04:09:40 +0800

    Update mpm99_3d.py

--- mpm99_3d.py
+++ mpm99_3d.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 import taichi as ti
@@ -121,8 +120,8 @@ def copy_color(np_c: ti.types.ndarray(), input_c: ti.types.ndarray()):
 @ti.kernel
 def initialize():
     for i in range(n_particles):  # 初始化粒子的位置
-        x[i] = [ti.random() * 0.1 + 0.3 + 0.08 * (i // group_size),  # 材质块x长度(随机*x偏移量） + 所有粒子的在x轴位置 + 材质块x轴的相对间隔(x偏移量*(粒子i//材质块粒子数，粒子i超过材质块粒子数时说明在下一材质块内，偏移一个整数量))
-                ti.random() * 0.2 + 0.05 + 0.32 * (i // group_size),  # 材质块y长度(随机*y偏移量） + 所有粒子的在y轴位置 + 材质块y轴的相对间隔(y偏移量*(例如:材质块粒子数=21845, 21845//21845=1, 粒子在第二个材质块偏移量为1))
+        x[i] = [ti.random() * 0.1 + 0.3 + 0.05 * (i // group_size),  # 材质块x长度(随机*x偏移量） + 所有粒子的在x轴位置 + 材质块x轴的相对间隔(x偏移量*(粒子i//材质块粒子数，粒子i超过材质块粒子数时说明在下一材质块内，偏移一个整数量))
+                ti.random() * 0.2 + 0.1 + 0.32 * (i // group_size),  # 材质块y长度(随机*y偏移量） + 所有粒子的在y轴位置 + 材质块y轴的相对间隔(y偏移量*(例如:材质块粒子数=21845, 21845//21845=1, 粒子在第二个材质块偏移量为1))
                 ti.random() * 0.2 + 0.3 + 0.1 * (i // group_size)]  # 材质块z长度(随机*z偏移量） + 所有粒子的在z轴位置 + 材质块z轴的相对间隔(z偏移量*(材质块ID))
         material[i] = i // group_size  # 材质块ID 0:流体; 1:果冻; 2:雪;
         v[i] = ti.Matrix([0, 0, 0])  # 初始化速度
@@ -131,7 +130,7 @@ def initialize():
 
 
 def T(a):  # 视角投影变换
-    phi, theta = np.radians(28), np.radians(32)  # 左右角度， 上下角度
+    phi, theta = np.radians(28), np.radians(30)  # 左右角度， 上下角度
 
     a = a - 0.5
     x, y, z = a[:, 0], a[:, 1], a[:, 2]
